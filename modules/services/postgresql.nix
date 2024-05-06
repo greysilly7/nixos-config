8325d29983001a72d7d3d81f8a13c9e64bcc9c35
Scott Gould <greysilly7@gmail.com>
2024-05-05 23:41:22 -0400

    Add PostgreSQL service configuration
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

---
+++ modules/services/postgresql.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  sops,
+  ...
+}: {
+  config = {
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = ["vaultwarden"];
+      ensureUsers = [
+        {
+          name = "vaultwarden";
+          ensureDBOwnership = true;
+        }
+      ];
+
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  auth-method
+        local all       all     trust
+      '';
+    };
+  };
+}
