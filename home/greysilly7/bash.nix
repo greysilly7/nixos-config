a31be4c41a2548002ffb175a0ee28bf1abc33c49
Scott Gould <greysilly7@gmail.com>
2024-05-06 09:34:01 -0400

    Fix interactive shell initialization in bash.nix
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- home/greysilly7/bash.nix
+++ home/greysilly7/bash.nix
@@ -6,7 +6,7 @@
       ll = "ls -l";
     };
 
-    interactiveShellInit = ''
+    initExtra = ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
       then
         shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
