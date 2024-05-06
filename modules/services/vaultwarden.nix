4b7b7bbe7a585f3930ff73fab9612e84047c24c1
Scott Gould <greysilly7@gmail.com>
2024-05-05 23:41:27 -0400

    Add Vaultwarden service configuration
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

---
+++ modules/services/vaultwarden.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  sops,
+  ...
+}: {
+  config = {
+    services.vaultwarden.enable = true;
+    services.vaultwarden.config = {
+      DOMAIN = "vaultwarden.greysilly7.net";
+
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+      ROCKET_LOG = "critical";
+
+      ADMIN_TOKEN = toString sops.vaultwarden.admin_token;
+    };
+  };
+}
