ba6e0319e166a5ed425b2ea118495977e45d5888
Scott Gould <greysilly7@gmail.com>
2024-05-11 02:11:00 -0400

    chore: Remove sops.age.keyFile and sops.age.generateKey in sops.nix
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- hosts/greyserver/sops.nix
+++ hosts/greyserver/sops.nix
@@ -9,9 +9,6 @@
   sops.age.sshKeyPaths = [
     "/etc/ssh/ssh_host_ed25519_key"
   ];
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  # This will generate a new key if the key specified above does not exist
-  sops.age.generateKey = true;
 
   sops.secrets = {
     grey_pass = {};
