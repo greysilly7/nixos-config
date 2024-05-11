ece9ec83890c7fff81161c8c9ad8183e72c7faa3
Scott Gould <greysilly7@gmail.com>
2024-05-11 14:46:55 -0400

    chore: Remove unused wireless.nix file and comment out cloudflared service in greyserver configuration.nix
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -6,13 +6,9 @@
   ...
 }: {
   config = {
-    networking.firewall.allowedTCPPorts = [22];
-    networking.firewall.allowedUDPPorts = [22];
-
     services.openssh = {
       enable = true;
       settings = {
-        X11Forwarding = lib.mkForce true;
         # root user is used for remote deployment, so we need to allow it
         PermitRootLogin = lib.mkForce "prohibit-password";
         PasswordAuthentication = lib.mkForce false; # disable password login
