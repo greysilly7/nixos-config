df0bedb67ce7408b7253d68a94d84ab914632867
Scott Gould <greysilly7@gmail.com>
2024-05-14 20:00:35 -0400

    chore: Update sops.nix to use correct path for defaultSopsFile
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- modules/sops.nix
+++ modules/sops.nix
@@ -5,7 +5,7 @@
   inputs,
   ...
 }: {
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.age.sshKeyPaths = [
     "/etc/ssh/ssh_host_ed25519_key"
   ];
