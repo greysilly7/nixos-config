46c0e3795a0f7a3894e976e88eba13003e2daa92
Scott Gould <greysilly7@gmail.com>
2024-05-15 22:35:46 -0400

    Update tabby service configuration to open firewall for HTTP and HTTPS
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- modules/services/tabby.nix
+++ modules/services/tabby.nix
@@ -3,5 +3,9 @@
     services.tabby = {
       enable = true;
     };
+
+    # Open Firewall for HTTP and HTTPS
+    networking.firewall.allowedTCPPorts = [11029];
+    networking.firewall.allowedUDPPorts = [11029];
   };
 }
