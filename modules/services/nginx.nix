229e4a50f0f4caee86b8815304676f595ef31b63
Scott Gould <greysilly7@gmail.com>
2024-05-23 22:21:49 -0400

    Update nginx.nix to proxy pass to AdGuard service with interpolated address
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -73,7 +73,7 @@
             deny all;
           '';
         };
-        location."/dns-query" = {
+        locations."/dns-query" = {
           proxyPass = "http://127.0.0.1:3000/dns-query";
         };
       };
