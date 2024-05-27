379d96ec6fde3f54a0457571a09670f101bcccd9
Scott Gould <greysilly7@gmail.com>
2024-05-27 12:29:59 -0400

    chore: Add comments
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- hosts/greypersonal/configuration.nix
+++ hosts/greypersonal/configuration.nix
@@ -7,50 +7,62 @@
   sops,
   ...
 }: {
+  # Import hardware configurations
   imports = [
+    # Local hardware configurations
     ./hardware-configuration.nix
-    ../../modules/hardware/bootloader.nix
-    ./i18n.nix
     ./tailscale.nix
-    ../../modules/sops.nix
+    ./i18n.nix
 
+    # Common and Nix Home Manager configurations
     ../../modules/common.nix
     ../../modules/nh.nix
 
+    # Desktop and gaming configurations
     ../../modules/desktop
     ../../modules/desktop/gaming
     ../../modules/desktop/gaming/steam.nix
 
+    # Hardware configurations
+    ../../modules/hardware/bootloader.nix
     ../../modules/hardware/intel.nix
     ../../modules/hardware/pipewire.nix
     ../../modules/hardware/power.nix
     ../../modules/hardware/bluetooth.nix
 
-    ../../modules/services/zram.nix
-    ../../modules/services/ssh.nix
-    ../../modules/services/printing.nix
-    ../../modules/services/encrypted_dns.nix
+    # SOPS configurations
+    ../../modules/sops.nix
+
+    # System service configurations
+    ../../modules/services/system/zram.nix
+    ../../modules/services/system/ssh.nix
+    ../../modules/services/system/printing.nix
+    ../../modules/services/system/encrypted_dns.nix
   ];
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
-  networking.hostName = "greypersonal";
+  # Kernel configurations
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen; # Use the Zen kernel
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Network configurations
+  networking.hostName = "greypersonal"; # Set the hostname
+  networking.networkmanager.enable = true; # Enable NetworkManager
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet # Add NetworkManager applet to system packages
+  ];
 
+  # Bootloader configurations
+  boot.loader.systemd-boot.enable = true; # Enable systemd-boot
+  boot.loader.efi.canTouchEfiVariables = true; # Allow bootloader to modify EFI variables
+
+  # Hardware configurations
   hardware = {
-    cpu.intel.updateMicrocode = true;
-    enableRedistributableFirmware = true;
+    cpu.intel.updateMicrocode = true; # Enable Intel microcode updates
+    enableRedistributableFirmware = true; # Enable redistributable firmware
   };
 
-  # Enable network manager
-  networking.networkmanager.enable = true;
-  environment.systemPackages = with pkgs; [
-    networkmanagerapplet
-  ];
+  # Cache configurations
+  chaotic.nyx.cache.enable = true; # Enable NYX cache
 
-  # WE want NYX Cache
-  chaotic.nyx.cache.enable = true;
-
-  system.stateVersion = "24.05";
+  # System state version
+  system.stateVersion = "24.05"; # Set the system state version
 }
--- hosts/greyserver/configuration.nix
+++ hosts/greyserver/configuration.nix
@@ -7,20 +7,25 @@
   sops,
   ...
 }: {
+  # Import hardware configurations
   imports = [
+    # Local hardware configurations
     ./hardware-configuration.nix
     ./tailscale.nix
 
+    # Common and Nix Home Manager configurations
     ../../modules/common.nix
     ../../modules/nh.nix
 
+    # Bootloader and SOPS configurations
     ../../modules/hardware/bootloader.nix
     ../../modules/sops.nix
 
-    ../../modules/services/zram.nix
-    ../../modules/services/ssh.nix
+    # System service configurations
+    ../../modules/services/system/zram.nix
+    ../../modules/services/system/ssh.nix
     ../../modules/services/nginx.nix
-    ../../modules/services/podman.nix
+    # ../../modules/services/podman.nix
     ../../modules/services/cloudflared.nix
     ../../modules/services/vaultwarden.nix
     ../../modules/services/postgresql.nix
@@ -28,16 +33,20 @@
     ../../modules/services/adguard.nix
   ];
 
-  networking.hostName = "greyserver";
-  networking.networkmanager.enable = true;
+  # Network configurations
+  networking.hostName = "greyserver"; # Set the hostname
+  networking.networkmanager.enable = true; # Enable NetworkManager
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader configurations
+  boot.loader.systemd-boot.enable = true; # Enable systemd-boot
+  boot.loader.efi.canTouchEfiVariables = true; # Allow bootloader to modify EFI variables
 
+  # Hardware configurations
   hardware = {
-    cpu.amd.updateMicrocode = true;
-    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = true; # Enable AMD microcode updates
+    enableRedistributableFirmware = true; # Enable redistributable firmware
   };
 
-  system.stateVersion = "24.05";
+  # System state version
+  system.stateVersion = "24.05"; # Set the system state version
 }
