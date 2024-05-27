cd0c51cb66e3ffd10907400c1f87e8c9a56837d3
Scott Gould <greysilly7@gmail.com>
2024-05-26 23:56:21 -0400

    fix: :bug: Wrap dnscrpyt settings in settings block
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- modules/services/encrypted_dns.nix
+++ modules/services/encrypted_dns.nix
@@ -8,20 +8,22 @@
 
     services.dnscrypt-proxy2 = {
       enable = true;
-      ipv6_servers = true;
-      require_dnssec = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
 
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+
+        # Use the Cloudflare DNS server
+        server_names = ["cloudflare" "cloudflare-ipv6"];
       };
-
-      # Use the Cloudflare DNS server
-      server_names = ["cloudflare" "cloudflare-ipv6"];
     };
 
     systemd.services.dnscrypt-proxy2.serviceConfig = {
