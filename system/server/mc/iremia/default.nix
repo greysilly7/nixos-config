cbc65d689f2248654685dc69a05c8e2042ef92d0
Scott Gould <greysilly7@gmail.com>
2024-06-27 00:13:48 -0400

    chore: Update default.nix to fix server configuration
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- system/server/mc/iremia/default.nix
+++ system/server/mc/iremia/default.nix
@@ -1,13 +1,20 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   modpack = pkgs.fetchPackwizModpack {
     url = "https://github.com/Alpaca-Industries/iremia-comeback/raw/1.0.0/pack.toml";
     packHash = "sha256-i+W0WA/eH/tNmYzguP2/v/rJb1LUnCV4WmMO9FpjcdI=";
   };
+  mcVersion = modpack.manifest.versions.minecraft;
+  fabricVersion = modpack.manifest.versions.fabric;
+  serverVersion = lib.replaceStrings ["."] ["_"] "fabric-${mcVersion}";
 in {
   services.minecraft-servers.servers.iremia = {
     enable = true;
     autoStart = true;
-    package = pkgs.fabricServers.fabric-1_21;
+    package = pkgs.fabricServers.${serverVersion}.override {loaderVersion = fabricVersion;};
 
     serverProperties = {
       server-port = 43000;
