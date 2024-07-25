cbf5f7b87d4b7636cba47f271d2fd48385539b68
Scott Gould <greysilly7@gmail.com>
2024-07-25 00:32:33 -0400

    feat: Update KDE and VirtManager configurations
    This commit updates the KDE and VirtManager configurations in the `default.nix` file. It enables SDDM display manager and Plasma 6 desktop manager for KDE. It also adds the necessary configurations for VirtManager, including installing required packages, enabling spice-vdagentd, and configuring virtualization settings.
    
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- hosts/greypersonal/default.nix
+++ hosts/greypersonal/default.nix
@@ -22,10 +22,37 @@
     extraPackages32 = with pkgs.pkgsi686Linux; [pkgs.mesa32_git.opencl intel-media-driver intel-vaapi-driver];
   };
 
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  # KDE
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
+  # VirtManager
+  # Enable virtualization with qemu and virt-manager and add the admin user to the necessary groups
+  users.users."greysilly7".extraGroups = [ "kvm" "input" "libvirtd" ];
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    win-spice
+    win-virtio
+    spice
+    spice-gtk
+    spice-protocol
+  ];
+  services.spice-vdagentd.enable = true;
+  virtualisation = {
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
+      # allowedBridges = ["wlo1
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+  };
+  # Kernel
   boot.kernelPackages = pkgs.linuxPackages_zen;
   # chaotic.scx.enable = true; # by default uses scx_rustland scheduler
 
