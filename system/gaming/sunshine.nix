81677a9877b4f8b61fca9aabe709ecf3695814a6
Scott Gould <greysilly7@gmail.com>
2024-07-27 14:40:34 -0400

    chore: add udev rule for sunshine

--- system/gaming/sunshine.nix
+++ system/gaming/sunshine.nix
@@ -5,4 +5,9 @@
     capSysAdmin = true;
     openFirewall = true;
   };
+
+  services.udev.extraRules = ''
+    # Allow users in the input group to access the /dev/input/event* devices
+    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
+  '';
 }
