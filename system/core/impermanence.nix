c67647d69be7d0f08eaafd424549c209e5252646
Scott Gould <greysilly7@gmail.com>
2024-08-14 01:13:40 -0400

    chore: Update impermanence.nix to improve rollback functionality
    This commit updates the impermanence.nix file to add a systemd service for rolling back the BTRFS root subvolume to a pristine state. The service mounts the BTRFS filesystem, deletes all subvolumes except /root, restores a blank /root subvolume, and then unmounts the filesystem. This improves the rollback functionality and ensures a clean state for the root subvolume.
    
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- system/core/impermanence.nix
+++ system/core/impermanence.nix
@@ -2,9 +2,7 @@
   inputs,
   lib,
   ...
-}: let
-  device = "/dev/mapper/crypted";
-in {
+}: {
   imports = [inputs.impermanence.nixosModule];
   programs.fuse.userAllowOther = true;
   environment.persistence."/persist" = {
@@ -17,7 +15,7 @@ in {
         "/var/db/sudo"
       ]
       ++ lib.forEach ["nixos" "NetworkManager" "nix" "ssh" "secureboot"] (x: "/etc/${x}")
-      ++ lib.forEach ["bluetooth" "nixos" "pipewire" "libvirt" "fail2ban" "fprint" "sops-nix" "sddm" "cups" "upower" "fwupd"] (x: "/var/lib/${x}");
+      ++ lib.forEach ["bluetooth" "tailscale" "nixos" "pipewire" "libvirt" "fail2ban" "fprint" "sops-nix" "sddm" "cups" "upower" "fwupd"] (x: "/var/lib/${x}");
     files = ["/etc/machine-id"];
   };
   # for some reason *this* is what makes networkmanager not get screwed completely instead of the impermanence module
@@ -35,44 +33,42 @@ in {
     unitConfig.DefaultDependencies = "no";
     serviceConfig.Type = "oneshot";
     script = ''
-      set -x
-
-      echo "Creating mount directory..."
       mkdir -p /mnt
 
-      echo "Mounting BTRFS filesystem..."
-      mount -t btrfs -o subvol=/ ${device} /mnt
-      if [ $? -ne 0 ]; then
-        echo "Failed to mount ${device} on /mnt"
-        exit 1
-      fi
+      # We first mount the btrfs root to /mnt
+      # so we can manipulate btrfs subvolumes.
+      mount -o subvol=/ /dev/mapper/crypted /mnt
 
-      echo "Listing and deleting subvolumes..."
-      btrfs subvolume list -o /mnt |
-        grep 'path' |
-        cut -f9 -d' ' |
-        while read subvolume; do
-          echo "Deleting /$subvolume subvolume..."
-          btrfs subvolume delete "/mnt/$subvolume"
-          if [ $? -ne 0 ]; then
-            echo "Failed to delete subvolume /mnt/$subvolume"
-          fi
-        done
-
-      echo "Deleting /root subvolume..."
+      # While we're tempted to just delete /root and create
+      # a new snapshot from /root-blank, /root is already
+      # populated at this point with a number of subvolumes,
+      # which makes `btrfs subvolume delete` fail.
+      # So, we remove them first.
+      #
+      # /root contains subvolumes:
+      # - /root/var/lib/portables
+      # - /root/var/lib/machines
+      #
+      # I suspect these are related to systemd-nspawn, but
+      # since I don't use it I'm not 100% sure.
+      # Anyhow, deleting these subvolumes hasn't resulted
+      # in any issues so far, except for fairly
+      # benign-looking errors from systemd-tmpfiles.
+      btrfs subvolume list -o /mnt/root |
+      cut -f9 -d' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done &&
+      echo "deleting /root subvolume..." &&
       btrfs subvolume delete /mnt/root
-      if [ $? -ne 0 ]; then
-        echo "Failed to delete /mnt/root subvolume"
-        exit 1
-      fi
 
-      echo "Restoring blank /root subvolume..."
+      echo "restoring blank /root subvolume..."
       btrfs subvolume snapshot /mnt/root-blank /mnt/root
 
-      echo "Unmounting /mnt..."
+      # Once we're done rolling back to a blank snapshot,
+      # we can unmount /mnt and continue on the boot process.
       umount /mnt
-
-      echo "Rollback completed successfully."
     '';
   };
 
