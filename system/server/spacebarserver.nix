d0e2a81e56338554e3a1400a04299c8fd141af05
Scott Gould <greysilly7@gmail.com>
2024-08-15 14:42:53 -0400

    ....

--- system/server/spacebarserver.nix
+++ system/server/spacebarserver.nix
@@ -48,7 +48,7 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:3001";
         extraConfig = ''
-          add_header_redefinition Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
+          add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
         '';
       };
     };
