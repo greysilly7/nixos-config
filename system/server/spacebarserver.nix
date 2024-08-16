40f2e88545aca115abb251bfe7aa9696c1280180
Scott Gould <greysilly7@gmail.com>
2024-08-16 00:19:49 -0400

    fix: make envfiles for imagor an array

--- system/server/spacebarserver.nix
+++ system/server/spacebarserver.nix
@@ -68,7 +68,7 @@
     containers = {
       imagor = {
         image = "shumc/imagor";
-        environmentFiles = config.sops.secrets.imagorenv.path;
+        environmentFiles = [config.sops.secrets.imagorenv.path];
         ports = [
           "8000:8000"
         ];
