89e3ed2ee64517c9d7e8de322613c2f350bc58b8
Scott Gould <greysilly7@gmail.com>
2024-09-08 00:35:33 -0400

    trying colema

--- flake.nix
+++ flake.nix
@@ -62,6 +62,9 @@
     flake-parts,
     ...
   }:
+  let
+    conf = self.nixosConfigurations;
+  in
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         # To import a flake module
@@ -91,15 +94,15 @@
         # those are more easily expressed in perSystem.
 
         nixosConfigurations = import ./hosts inputs;
-        colmena = {
-          meta = {
-            description = "my personal machines";
-            # This can be overriden by node nixpkgs
-            nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-            nodeNixpkgs = builtins.mapAttrs (name: value: value.pkgs) conf;
-            nodeSpecialArgs = builtins.mapAttrs (name: value: value._module.specialArgs) conf;
-          };
-        } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) conf;
-      };
+          colmena = {
+            meta = {
+              description = "my personal machines";
+              # This can be overriden by node nixpkgs
+              nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+              nodeNixpkgs = builtins.mapAttrs (name: value: value.pkgs) conf;
+              nodeSpecialArgs = builtins.mapAttrs (name: value: value._module.specialArgs) conf;
+            };
+          } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) conf;
+        };
     };
 }
