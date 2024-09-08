f1f89eabbb4902c6506ec3fe0d1edd79faebcf61
Scott Gould <greysilly7@gmail.com>
2024-09-08 00:33:00 -0400

    trying colema

--- flake.nix
+++ flake.nix
@@ -91,6 +91,15 @@
         # those are more easily expressed in perSystem.
 
         nixosConfigurations = import ./hosts inputs;
+        colmena = {
+          meta = {
+            description = "my personal machines";
+            # This can be overriden by node nixpkgs
+            nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+            nodeNixpkgs = builtins.mapAttrs (name: value: value.pkgs) conf;
+            nodeSpecialArgs = builtins.mapAttrs (name: value: value._module.specialArgs) conf;
+          };
+        } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) conf;
       };
     };
 }
