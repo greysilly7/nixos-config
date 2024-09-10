393843ca726cd091b14af527bf0197650273f81d
Scott Gould <greysilly7@gmail.com>
2024-09-10 00:15:11 -0400

    .

--- system/server/githubCi.nix
+++ system/server/githubCi.nix
@@ -1,8 +1,9 @@
 {config, ...}: {
   services.github-nix-ci = {
+    age.secretsDir = null;
     personalRunners = {
-    tokenFile = config.sops.secrets.github_ci_token.path;
-    "greysilly7/nixos-config".num = 10;
+      tokenFile = config.sops.secrets.github_ci_token.path;
+      "greysilly7/nixos-config".num = 10;
     };
   };
 }
