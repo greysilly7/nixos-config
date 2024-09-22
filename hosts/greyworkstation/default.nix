6c9946337ccac35afc2d63763f9ea955a46ed1fb
Scott Gould <greysilly7@gmail.com>
2024-09-22 18:48:34 -0400

    add open=false

--- hosts/greyworkstation/default.nix
+++ hosts/greyworkstation/default.nix
@@ -49,11 +49,16 @@
       services.xserver.videoDrivers = ["nvidia"];
       hardware.opengl.enable = true;
 
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+      hardware.nvidia = {
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
 
-      # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-      hardware.nvidia.modesetting.enable = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+
+        modesetting.enable = true;
+        # Use opensource drivers?
+        open = false;
+      };
 
       hardware.nvidia.prime = {
         sync.enable = true;
