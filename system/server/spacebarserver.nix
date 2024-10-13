0f215e30f171c95494412700add8ee3ec54d721b
Scott Gould <greysilly7@gmail.com>
2024-10-13 19:52:00 -0400

    bug: Fix formatting
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- system/server/spacebarserver.nix
+++ system/server/spacebarserver.nix
@@ -67,17 +67,17 @@
         };
       };
     };
+  };
 
-    virtualisation.oci-containers = {
-      backend = "docker";
-      containers = {
-        imagor = {
-          image = "shumc/imagor";
-          environmentFiles = [config.sops.secrets.imagorenv.path];
-          ports = [
-            "8000:8000"
-          ];
-        };
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      imagor = {
+        image = "shumc/imagor";
+        environmentFiles = [config.sops.secrets.imagorenv.path];
+        ports = [
+          "8000:8000"
+        ];
       };
     };
   };
