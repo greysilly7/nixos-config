380b155a17bab33f39b69203bcd56193bcffeedc
Scott Gould <greysilly7@gmail.com>
2024-10-24 08:54:29 -0400

    Fix nginx virtual host configuration for spacebar.greysilly7.xyz
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- system/server/spacebarchat/imagor.nix
+++ system/server/spacebarchat/imagor.nix
@@ -12,7 +12,7 @@
     };
   };
 
-  services.nginx.virtualHosts."spacebar.greysilly7.xyz" = {
+  services.nginx.virtualHosts."spacebar.greysilly7.xyz".locations = {
     "/media" = {
       proxyPass = "http://127.0.0.1:8000";
     };
