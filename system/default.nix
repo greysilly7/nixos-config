07cc69f20027f4040c87bdb3da9403468c3857b1
Scott Gould <greysilly7@gmail.com>
2026-02-16 23:36:35 -0500

    Update flake

--- system/default.nix
+++ system/default.nix
@@ -24,6 +24,46 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
+  security = {
+    polkit.enable = true;
+    sudo-rs.enable = true;
+  };
+  services.userborn.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.X11Forwarding = false;
+    settings.KbdInteractiveAuthentication = false;
+    settings.PasswordAuthentication = false;
+    settings.UseDns = false;
+    # unbind gnupg sockets if they exists
+    settings.StreamLocalBindUnlink = true;
+
+    # Use key exchange algorithms recommended by `nixpkgs#ssh-audit`
+    settings.KexAlgorithms = [
+      "mlkem768x25519-sha256"
+      "sntrup761x25519-sha512"
+      "sntrup761x25519-sha512@openssh.com"
+      "curve25519-sha256"
+      "curve25519-sha256@libssh.org"
+      "diffie-hellman-group16-sha512"
+      "diffie-hellman-group18-sha512"
+    ];
+  };
+
+  programs.ssh.knownHosts = {
+    "github.com".hostNames = [ "github.com" ];
+    "github.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+
+    "gitlab.com".hostNames = [ "gitlab.com" ];
+    "gitlab.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+
+    "git.sr.ht".hostNames = [ "git.sr.ht" ];
+    "git.sr.ht".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+  };
+
   environment.etc.machine-id.text = "796f7520617265206175746973746963";
   time.timeZone = "America/Detroit";
   system.stateVersion = "25.11";
---
+++ system/nix/default.nix
@@ -0,0 +1,30 @@
+{ lib, ... }:
+{
+  imports = [
+    ./extra-substituters.nix
+  ];
+  nix = {
+    channel.enable = false;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+      trusted-users = [
+        "@wheel"
+        "root"
+      ];
+      log-lines = 25;
+      max-free = lib.mkDefault (3000 * 1024 * 1024);
+      min-free = lib.mkDefault (512 * 1024 * 1024);
+      builders-use-substitutes = true;
+      connect-timeout = 5;
+      fallback = true;
+    };
+  };
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
+
+}
