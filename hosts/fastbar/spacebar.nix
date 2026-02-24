73ffffc4985a212359c0c839c193cb87cd5cedee
Scott Gould <greysilly7@gmail.com>
2026-02-24 18:44:09 -0500

    Update SB settings

--- hosts/fastbar/spacebar.nix
+++ hosts/fastbar/spacebar.nix
@@ -27,23 +27,37 @@ in
     legacyJwtSecretPath = "/secrets/spacebar/legacyJwtSecret";
     # mailjetApiKeyPath = "/run/secrets/spacebar/mailjetApiKey";
     # mailjetApiSecretPath = "/run/secrets/spacebar/mailjetApiSecret";
-    # smtpPasswordPath = "/run/secrets/spacebar/smtpPassword";
+    smtpPasswordPath = "/secrets/spacebar/smtpPassword";
     # gifApiKeyPath = "/run/secrets/spacebar/gifApiKey";
     # rabbitmqHostPath = "/run/secrets/spacebar/rabbitmqHost";
     # abuseIpDbApiKeyPath = "/run/secrets/spacebar/abuseIpDbApiKey";
-    # captchaSecretKeyPath = "/run/secrets/spacebar/captchaSecretKey";
-    # captchaSiteKeyPath = "/run/secrets/spacebar/captchaSiteKey";
+    captchaSecretKeyPath = "/secrets/spacebar/captchaSecretKey";
+    captchaSiteKeyPath = "/secrets/spacebar/captchaSiteKey";
     # ipdataApiKeyPath = "/run/secrets/spacebar/ipdataApiKey";
     requestSignaturePath = "/secrets/spacebar/requestSignature";
 
     settings = {
+      email = {
+        provider = "smtp";
+        senderAddress = "no-reply@mail.greysilly7.xyz";
+        smtp = {
+          host = "smtp.mailgun.org";
+          port = 465;
+          secure = true;
+          username = "no-reply@mail.greysilly7.xyz";
+        };
+      };
       security = {
         forwardedFor = "X-Forwarded-For";
-        trustedProxies = "192.168.0.80, linklocal";
+        trustedProxies = "192.168.0.80";
         cdnSignUrls = true;
         cdnSignatureIncludeIp = true;
         cdnSignatureIncludeUserAgent = true;
         cdnSignatureDuration = "5m";
+        captcha = {
+          enabled = true;
+          service = "recaptcha";
+        };
       };
       general = {
         frontPage = "https://spacebar.greysilly7.xyz";
@@ -59,9 +73,18 @@ in
         autoJoin = {
           bots = false;
           canLeave = true;
-          enabled = false;
-          guilds = [ "1006649183970562092" ];
+          enabled = true;
+          guilds = [ "1475999262805341752" ];
         };
+        discovery = {
+          showAllGuilds = false;
+        };
+      };
+      register = {
+        email.required = true;
+        dateOfBirth.minimum = 16;
+        password.required = true;
+        requireCaptcha = true;
       };
       user = {
         blockedContains = [
@@ -85,6 +108,88 @@ in
           "outlook.com"
           "steam"
         ];
+      };
+            limits = {
+        guild = {
+          maxMembers = 25000000;
+          maxEmojis = 2000;
+          maxChannelsInCategory = 65535;
+          maxChannels = 250;
+          maxRoles = 250;
+          maxBulkBanUsers = 200;
+          maxStickers = 500;
+        };
+        message = {
+          maxCharacters = 1048576;
+          maxTTSCharacters = 160;
+          maxReactions = 2048;
+          maxAttachmentSize = 1073741824;
+          maxEmbedDownloadSize = 5242880;
+          maxBulkDelete = 1000;
+          maxPreloadCount = 100;
+        };
+        channel = {
+          maxPins = 500;
+          maxTopic = 1024;
+          maxWebhooks = 100;
+        };
+        rate = {
+          ip = {
+            window = 5;
+            count = 500;
+          };
+          global = {
+            count = 250;
+            window = 5;
+          };
+          error = {
+            window = 5;
+            count = 10;
+          };
+          routes = {
+            guild = {
+              window = 5;
+              count = 5;
+            };
+            webhook = {
+              count = 10;
+              window = 5;
+            };
+            channel = {
+              count = 10;
+              window = 5;
+            };
+            auth = {
+              login = {
+                window = 60;
+                count = 5;
+              };
+              register = {
+                count = 2;
+                window = 43200;
+              };
+            };
+          };
+          enabled = false;
+        };
+        user = {
+          maxGuilds = 1000;
+          maxUsername = 64;
+          maxFriends = 2000;
+          maxBio = 500;
+        };
+        absoluteRate = {
+          register = {
+            limit = 25;
+            window = 3600000;
+            enabled = false;
+          };
+          sendMessage = {
+            limit = 120;
+            window = 60000;
+            enabled = false;
+          };
+        };
       };
     };
     extraEnvironment = {
