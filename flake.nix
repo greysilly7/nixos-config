3be1d152042e554c2b26202664af083bd82342bb
Scott Gould <greysilly7@gmail.com>
2026-02-24 14:22:40 -0500

    .

--- flake.nix
+++ flake.nix
@@ -4,17 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/25.11";
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
-
     nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
     disko.url = "github:nix-community/disko";
 
@@ -44,7 +33,6 @@
         laptop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            lix-module.nixosModules.default
             nixos-facter-modules.nixosModules.facter
             disko.nixosModules.default
             sops-nix.nixosModules.sops
@@ -70,7 +58,6 @@
         fastbar = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            lix-module.nixosModules.default
             sops-nix.nixosModules.sops
             ./hosts/fastbar/configuration.nix
 
