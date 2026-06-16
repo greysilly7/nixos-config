4441b1dabb4ae00f65875a996f8f574837b5dcc7
Scott Gould <greysilly7@gmail.com>
2026-06-16 06:12:34 -0400

    refactor: simplify nodejs aspect (YAGNI)

--- modules/programs/dev/nodejs.nix
+++ modules/programs/dev/nodejs.nix
@@ -1,26 +1,10 @@
-{ den, ... }:
+_:
 {
-  den.aspects.dev._.nodejs = {
-    includes = [
-      den.aspects.dev._.nodejs._.enable
-    ];
-
-    _.enable = _: {
-      darwin =
-        { pkgs, ... }:
-        {
-          environment.systemPackages = [ pkgs.nodejs ];
-        };
-      nixos =
-        { pkgs, ... }:
-        {
-          environment.systemPackages = [ pkgs.nodejs ];
-        };
-      homeManager =
-        { pkgs, ... }:
-        {
-          home.packages = [ pkgs.nodejs ];
-        };
-    };
+  den.aspects.dev._.nodejs = _: {
+    darwin =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [ pkgs.nodejs ];
+      };
   };
 }
