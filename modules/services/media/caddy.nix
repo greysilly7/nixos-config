fb63ee2a71745e388b1946000d5c31dced2e17c6
Scott Gould <greysilly7@gmail.com>
2026-06-24 04:17:32 -0400

    .

--- modules/services/media/caddy.nix
+++ modules/services/media/caddy.nix
@@ -5,9 +5,6 @@ _: {
       services.caddy = {
         enable = true;
         virtualHosts = {
-          "seerr.greyserver".extraConfig = ''
-            reverse_proxy localhost:5055
-          '';
           "aiostreams.greysilly7.xyz".extraConfig = ''
             reverse_proxy localhost:3000
           '';
@@ -16,15 +13,6 @@ _: {
           '';
           "jellyfin.greysilly7.xyz".extraConfig = ''
             reverse_proxy localhost:8096
-          '';
-          "sonarr.greyserver".extraConfig = ''
-            reverse_proxy localhost:8989
-          '';
-          "radarr.greyserver".extraConfig = ''
-            reverse_proxy localhost:7878
-          '';
-          "lidarr.greyserver".extraConfig = ''
-            reverse_proxy localhost:8686
           '';
           "prowlarr.greysilly7.xyz".extraConfig = ''
             reverse_proxy localhost:9696
