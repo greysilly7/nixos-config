187f4a1f7cc440a6c289bf468f569053196377d3
Scott Gould <greysilly7@gmail.com>
2026-07-26 17:16:24 -0400

    Bind vaultwarden to tailscale

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -4,17 +4,12 @@ _: {
       services.vaultwarden = {
         enable = true;
         config = {
-          ROCKET_ADDRESS = "127.0.0.1";
+          ROCKET_ADDRESS = "100.74.250.64";
           ROCKET_PORT = 8222;
           DOMAIN = "https://vaultwarden.greysilly7.xyz";
           SIGNUPS_ALLOWED = true;
         };
       };
-
-      services.caddy.virtualHosts."vaultwarden.greysilly7.xyz".extraConfig = ''
-        import security_headers
-        reverse_proxy localhost:8222
-      '';
     };
   };
 }
