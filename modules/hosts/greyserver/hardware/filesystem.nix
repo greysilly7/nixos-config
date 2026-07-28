c02088bcb309507f7a67493016322e3c5c6be15e
Scott Gould <greysilly7@gmail.com>
2026-07-28 05:40:18 -0400

    add ssd to greyserver

--- modules/hosts/greyserver/hardware/filesystem.nix
+++ modules/hosts/greyserver/hardware/filesystem.nix
@@ -3,9 +3,18 @@ _: {
     disko = import ./_disko.nix;
 
     nixos = {
-      fileSystems = {
+      boot.supportedFilesystems = [ "btrfs" ];
+      fileSystems.fileSystems = {
         "/persist".neededForBoot = true;
         "/var/log".neededForBoot = true;
+        "/mnt/speedy" = {
+          device = "/dev/disk/by-uuid/26e8e2bb-267b-4bdb-8268-20ae841f3b88";
+          fsType = "btrfs";
+          options = [
+            "compress=zstd"
+            "nofail"
+          ];
+        };
       };
       swapDevices = [
         { device = "/dev/zvol/zroot/swap"; }
