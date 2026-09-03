4ad39a6b0fd4a02bc4903c2a8a5d27183fff8876
Scott Gould <greysilly7@gmail.com>
2026-09-03 07:04:36 +0000

    ;(

--- modules/services/nntp-proxy.nix
+++ modules/services/nntp-proxy.nix
@@ -1,6 +1,6 @@
 {...}: {
   den.aspects.nntp-proxy = {
-    flake-file.inputs.home-manager = {
+    flake-file.inputs.nntp-proxy = {
       url = "github:greysilly7/nntp-proxy";
       inputs.nixpkgs.follows = "nixpkgs";
     };
