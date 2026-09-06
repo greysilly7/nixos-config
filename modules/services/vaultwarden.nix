570fb0ef0db9b9968a881adde1683dd7684671ca
Scott Gould <greysilly7@gmail.com>
2026-09-06 04:53:29 +0000

    Enable vaultwarden signups

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -10,7 +10,7 @@ _: {
             ROCKET_ADDRESS = "0.0.0.0";
             ROCKET_PORT = 8222;
             DOMAIN = "https://vaultwarden.greysilly7.xyz";
-            SIGNUPS_ALLOWED = false;
+            SIGNUPS_ALLOWED = true;
           };
         };
       };
